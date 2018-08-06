0ae839d0548bcb654740b225be14059a601987b3
Rachel Hovell <r.hovell@live.com>
2018-08-06 20:38:57 +0100

    Added map API key code

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoicmhvdmVsbCIsImEiOiJjamtpNXVwMG4wcDFkM3Fwazdrb2R1ZW1iIn0.g6FH2qgoIOY7tli4cuIFxg',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -33,7 +33,7 @@ initMap = () => {
       DBHelper.mapMarkerForRestaurant(self.restaurant, self.newMap);
     }
   });
-}  
+}
 /**
  * Get current restaurant from page URL.
  */
