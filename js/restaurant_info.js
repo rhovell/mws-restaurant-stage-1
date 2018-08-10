feb22dcd53c796f49454b615b2bc48530196b389
Rachel Hovell <r.hovell@live.com>
2018-08-10 14:03:38 +0100

    Fourth URL test

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -167,12 +167,12 @@ fillBreadcrumb = (restaurant=self.restaurant) => {
 getParameterByName = (name, url) => {
   if (!url)
     url = window.location.href;
-  name = name.replace(/[\[\]]/, '\$&');
+  name = name.replace(/[[\]]/, '\\$&');
   const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`),
     results = regex.exec(url);
   if (!results)
     return null;
   if (!results[2])
     return '';
-  return decodeURIComponent(results[2].replace(/\, ' '));
+  return decodeURIComponent(results[2].replace(/\+/, ' '));
 }
