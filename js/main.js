268ce5f2155d6acc9ac254fbc01a8acbc7bc5cf5
Rachel Hovell <r.hovell@live.com>
2018-08-10 13:47:55 +0100

    Ninth image test

--- js/main.js
+++ js/main.js
@@ -152,7 +152,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.alt = restaurant.name + " Restaurant Image";
-  image.src = './' + DBHelper.imageUrlForRestaurant(restaurant);
+  image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
   const name = document.createElement('h2');
